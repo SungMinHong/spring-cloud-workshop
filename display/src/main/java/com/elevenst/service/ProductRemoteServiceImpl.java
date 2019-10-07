dcc8d8120371573a9ae20e9e161fbcc119ac4529
홍성민 <smhong@tmon.co.kr>
2019-10-08 01:03:00 +0900

    [step-2] hystrix 추가 및 서킷활성화

--- display/src/main/java/com/elevenst/service/ProductRemoteServiceImpl.java
+++ display/src/main/java/com/elevenst/service/ProductRemoteServiceImpl.java
@@ -4,6 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+
 /**
  * Created by smhong on 2019-10-06.
  */
@@ -15,6 +17,7 @@ public class ProductRemoteServiceImpl implements ProductRemoteService{
     private RestTemplate restTemplate;
 
     @Override
+    @HystrixCommand
     public String getProductInfo(String productId) {
         return restTemplate.getForObject(URL + productId, String.class);
     }
